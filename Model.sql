8143ed1e1ef4dae1209407a760d3e5149374452c
Javier Riveros <javiiriveros@hotmail.com>
2016-11-19 18:06:46 -0500

    Added integrates table to Model.sql

--- Model.sql
+++ Model.sql
@@ -35,6 +35,12 @@ CREATE TABLE students (
   incorporation_date timestamp
 );
 
+# Relations
+CREATE TABLE integrates (
+    teacher_ci integer,
+    tribunal_id integer
+);
+
 # Primary Keys
 ALTER TABLE groups ADD CONSTRAINT pk_groups PRIMARY KEY(id);
 ALTER TABLE projects ADD CONSTRAINT pk_projects PRIMARY KEY(order_number);
@@ -42,15 +48,15 @@ ALTER TABLE students ADD CONSTRAINT pk_students PRIMARY KEY(registration_number)
 ALTER TABLE teachers ADD CONSTRAINT pk_teachers PRIMARY KEY(ci);
 ALTER TABLE themes ADD CONSTRAINT pk_themes PRIMARY KEY(order_number);
 ALTER TABLE tribunals ADD CONSTRAINT pk_tribunals PRIMARY KEY(id);
+ALTER TABLE integrates ADD CONSTRAINT pk_integrates PRIMARY KEY(teacher_ci, tribunal_id);
 
-# Foreign Keys
 # Aditional Fields
 ALTER TABLE projects ADD COLUMN tribunal_id integer;
 ALTER TABLE students ADD COLUMN group_id integer;
 ALTER TABLE students ADD COLUMN order_number integer;
 ALTER TABLE students ADD COLUMN teacher_ci integer;
 
-# Constraints
+# Constraints & foreign keys
 
 ALTER TABLE themes ADD CONSTRAINT fk_themes_projects 
   FOREIGN KEY(order_number) REFERENCES projects(order_number) 
@@ -70,4 +76,12 @@ ALTER TABLE students ADD CONSTRAINT fk_students_teachers
 
 ALTER TABLE projects ADD CONSTRAINT fk_projects_tribunals
   FOREIGN KEY(tribunal_id) REFERENCES tribunals(id) 
+  ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+ALTER TABLE integrates ADD CONSTRAINT pk_integrates_teacher 
+  FOREIGN KEY(teacher_ci) REFERENCES teachers(ci)
+  ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+ALTER TABLE integrates ADD CONSTRAINT pk_integrates_tribunal 
+  FOREIGN KEY(tribunal_id) REFERENCES tribunals(id)
   ON DELETE RESTRICT ON UPDATE RESTRICT;
